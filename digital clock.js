8b8765ec645842ff282a0ba3306b9a9d698118bf
Rushikesh Kothawade <97045515+RushikeshKothawade07@users.noreply.github.com>
2023-02-10 16:06:03 +0530

    Create digital clock.js

---
+++ digital clock.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+const App = () => {
+  const state = useState();
+  let newTime = new Date().toLocaleTimeString();
+  const [ctime,setCTime]  =  useState(newTime);
+  const func = () => {
+    newTime = new Date().toLocaleTimeString();
+    setCTime(newTime)
+  };
+  setInterval(func,1000)
+  return (
+    <>
+      <h1>{newTime}</h1>
+      <button onClick={func}> Click me </button>
+    </>
+  );
+};
+
+export default App;
