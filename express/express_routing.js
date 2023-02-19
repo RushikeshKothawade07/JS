005dfb22d93d15e6f4c0add76240aad9c5811116
Rushikesh Kothawade <97045515+RushikeshKothawade07@users.noreply.github.com>
2023-02-19 19:41:06 +0530

    Create express_routing.js

---
+++ express/express_routing.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello world from express')
+})
+app.get('/about',(req,res)=>{
+    res.send('Hello world from about page of express')
+})
+app.listen(8000,()=>{
+    console.log('listening from 8000');
+})
